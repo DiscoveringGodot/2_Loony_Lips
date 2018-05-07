75aa61671414747e9533ed045f854d8e7e710a59
Yann Burrett <yann.let@gmail.com>
2018-05-07 14:50:14 -0400

    3  RichTextLabel - Showing Text to the Player

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,4 +3,5 @@ extends Node2D
 func _ready():
 	var prompt = ["Yann", "pizza", "full", "sad", "cabbages"]
 	var story = "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s."
-	print (story % prompt)
+	#$Blackboard/StoryText.text = story % prompt
+	$Blackboard/StoryText.bbcode_text = story % prompt
