62aee0a7df0f072fbd348538735b4b1e615c038b
Yann Burrett <yann.let@gmail.com>
2018-05-07 17:01:14 -0400

    4 - LineEdit - Text From The Player

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,5 +3,14 @@ extends Node2D
 func _ready():
 	var prompt = ["Yann", "pizza", "full", "sad", "cabbages"]
 	var story = "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s."
-	#$Blackboard/StoryText.text = story % prompt
-	$Blackboard/StoryText.bbcode_text = story % prompt
+	$Blackboard/StoryText.text = story % prompt
+	$Blackboard/TextBox.text = ""
+
+func _on_TextureButton_pressed():
+	var new_text = $Blackboard/TextBox.get_text()
+	_on_TextBox_text_entered(new_text)
+
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/StoryText.text = new_text
+	$Blackboard/TextBox.text = ""
+
