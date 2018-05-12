b532691d97ae1fa7affccc24b9d8e389e985bcca
Ben Tristem <github@bentristem.com>
2018-05-12 18:22:15 +0100

    Loading stories dictionary from JSON file

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -2,27 +2,22 @@ extends Node2D
 
 var player_words = [] # the words the player chooses
 
-var template = {
+var template = {}
 
 var current_story
 
 func _ready():
-	save_stories()
+	load_stories()
 	randomize()  # TODO Yann necessary?
 	current_story = template.values() [randi() % template.size()]
 	$Blackboard/StoryText.text = ("Welcome to Loony Lips!\n\nWe're going to tell a story and have a lovely time!\n\nCan I have " + current_story.prompt[player_words.size()] + ", please?")
 	$Blackboard/TextBox.text = ""
 
-func save_stories():
-	# Open a file
-	var file = File.new()
-	if file.open("test.json", File.WRITE) != 0:
-		print("Error opening file")
-		return
-
-	# Save the dictionary as JSON (or whatever you want, JSON is convenient here because it's built-in)
-	file.store_line(to_json(template))
-	file.close()
+func load_stories():
+	# TODO consdier checking for file existence
+	var stories_file = File.new()
+	stories_file.open("stories.json", File.READ)
+	template = parse_json(stories_file.get_line())
 
 func _on_TextureButton_pressed():
 	if is_story_done():
